d394add58c186121409f4c6a9fbb7bb826f57776
David Nagarpowers <dnagarpowers@gmail.com>
2026-05-25 16:12:29 -0400

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def rob1(nums: list[int]) -> int:
+            rob_1, rob_2 = 0, 0
+
+            for n in nums:
+                # Either you can rob current and two before
+                # or skip current house and just rob one before
+                rob_1, rob_2 = rob_2, max(n + rob_1, rob_2)
+            
+            return rob_2
+
+        # you need nums[0] in the case where there is only one element
+        # since both subarrays would then be empty
+        return max(nums[0], rob1(nums[:-1]), rob1(nums[1:]))

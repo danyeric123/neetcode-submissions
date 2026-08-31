3ebf2785ffffa1ada345b4c96d7f5b89cbc823a6
David Nagarpowers <dnagarpowers@gmail.com>
2026-08-31 15:19:04 -0400

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,36 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        """
+        Another way of looking at this is 
+        by going through time and seeing what
+        meetings are running at that time and 
+        the max number of meetings are the min
+        we need
+        """
+
+        meetings = defaultdict(int)
+
+        for interval in intervals:
+            meetings[interval.start] += 1
+            meetings[interval.end] -= 1
+        
+        prev = res = 0
+
+        for t in sorted(meetings.keys()):
+            # we run through the times
+            # and add number of concurrent meetings
+            # at that time
+            prev += meetings[t]
+
+            # then see if this is the max
+            res = max(res, prev)
+        
+        return res
